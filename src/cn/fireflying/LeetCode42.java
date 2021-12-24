b5ed333f5038946a9f2a8e7e54c1cb6c98242db3
fire flying <caogj@tcsec.com.cn>
2021-12-24 12:49:17 +0800

    196 213 746

--- src/cn/fireflying/LeetCode42.java
+++ src/cn/fireflying/LeetCode42.java
@@ -10,12 +10,6 @@ import java.util.Arrays;
 /**
  * 给定n 个非负整数表示每个宽度为 1 的柱子的高度图，计算按此排列的柱子，下雨之后能接多少雨水。
  *
- *
- *
- * 示例 1：
- *
- *
- *
  * 输入：height = [0,1,0,2,1,0,1,3,2,1,2,1]
  * 输出：6
  * 解释：上面是由数组 [0,1,0,2,1,0,1,3,2,1,2,1] 表示的高度图，在这种情况下，可以接 6 个单位的雨水（蓝色部分表示雨水）。
@@ -72,6 +66,33 @@ public class LeetCode42 {
             }
         }
         return finalInt;
+    }
+
+    public static int trap1(int[] height) {
+        int length = height.length;
+        if (length < 3) {
+            return 0;
+        }
+
+        int left = 0;
+        int right = length - 1;
+        int maxLeft = -1;
+        int maxRight = -1;
+
+        while (left < right) {
+            while (left < right && height[left] > maxLeft) {
+                maxLeft = height[left];
+                left++;
+            }
+
+            while (left < right && height[right] > maxRight) {
+                maxRight = height[right];
+                right--;
+            }
+        }
+
+        return 1;
+
     }
 
 
