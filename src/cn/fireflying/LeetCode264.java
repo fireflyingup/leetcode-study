c6d1860507d6cc2741b84573b9593761fdea5dae
fire flying <caogj@tcsec.com.cn>
2022-01-07 09:44:29 +0800

    264

--- src/cn/fireflying/LeetCode264.java
+++ src/cn/fireflying/LeetCode264.java
@@ -8,27 +8,31 @@ import java.util.Map;
 public class LeetCode264 {
 
     public static void main(String[] args) {
-        System.out.println(nthUglyNumber(1352));
+        System.out.println(nthUglyNumber(10));
     }
 
     public static int nthUglyNumber(int n) {
-        int count = 1;
-        Map<Integer, Boolean> map = new HashMap<>();
-        map.put(1, true);
-        int i = 1;
-        while (count < n) {
-            i++;
-            if (i % 2 == 0 && null != map.get(i / 2)) {
-                map.put(i, true);
-                count++;
-            } else if (i % 3 == 0 && null != map.get(i / 3)) {
-                map.put(i, true);
-                count++;
-            } else if (i % 5 == 0 && null != map.get(i / 5)) {
-                map.put(i, true);
-                count++;
+        int plus2 = 0;
+        int plus3 = 0;
+        int plus5 = 0;
+        int[] dp = new int[n];
+        dp[0] = 1;
+        int max = 1;
+        for (int i = 1; i < n; ) {
+            int temp = 0;
+            if (2 * dp[plus2] <= 3 * dp[plus3] && 2 * dp[plus2] <= 5 * dp[plus5]) {
+                temp = 2 * dp[plus2++];
+            } else if (3 * dp[plus3] <= 5 * dp[plus5]) {
+                temp = 3 * dp[plus3++];
+            } else {
+                temp = 5 * dp[plus5++];
+            }
+            if (temp > max) {
+                max = temp;
+                dp[i++] = max;
             }
         }
-        return i;
+        System.out.println(Arrays.toString(dp));
+        return dp[n - 1];
     }
 }
